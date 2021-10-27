ab12faed318160bf578b79c7c3d1522f32829526
Bandar93 <alshalahi93@outlook.com>
2021-10-27 19:29:43 +0300

    Done Middleware

--- apis/products/controllers.js
+++ apis/products/controllers.js
@@ -1,42 +1,49 @@
 const { json } = require("express");
 const Product = require("../../db/models/Products");
 
-const productlistFetch = async (req , res) => {
+const productlistFetch = async (req , res , next) => {
     try {
         const products = await Product.find();
         return res.json(products);
     } catch (error){
-        res.status(500).json({massage: error});
+        next(error)
+       // res.status(500).json({massage: error});
     }
     
 };
 
-const productCreate = async (req,res) => {
+const productCreate = async (req,res, next) => {
     try{
     const newProduct = await Product.create(req.body);
     // console.log(newProduct)
     res.status(201).json(newProduct)
     }catch (error){
-    res.status(500).json({massage: error});
+        next(error)
+    // res.status(500).json({massage: error});
     }
 };
 
-const productDelete = async (req,res) => {
+const productDelete = async (req,res,next) => {
     try {
     const product = await Product.findById(req.params.productId);
     if(product){
         await product.remove();
         return res.status(204).end();
     }else{
-        return res.status(404).json({massage: "error"});
+        next({
+            status: 404,
+            massage: "Product Not Found!",
+        })
+        // return res.status(404).json({massage: "error"});
     }
     
 } catch (error) {
-    res.status(500).json({massage: error});
+    next(error)
+    // res.status(500).json({massage: error});
 }
 }
 
-const productUpdate = async (req,res) => {
+const productUpdate = async (req,res,next) => {
     // const productId = res.params.productId;
     const { productId } = req.params;
     try{ 
@@ -49,11 +56,16 @@ const productUpdate = async (req,res) => {
         return res.json(product);
 
     }else {
-        return res.status(404),json({ massage: "Not Found"});
+        // return res.status(404),json({ massage: "Not Found"});
+        next({
+            status: 404,
+            massage: "Product Not Found!",
+        })
     }
 
     } catch (error) {
-        return res.status(500).json({massage: "error"})
+        next(error)
+        // return res.status(500).json({massage: "error"})
     }
 }
 
--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const productsRoutes = require("./apis/products/routes")
-// let products = require("./data")
+
 const app = express();
 const connectDB = require("./database")
 // Middleware
+
 app.use(express.json());
+// we can use morgan dev libary
+app.use((req,res,next) => {
+    console.log(req.originalUrl)
+    console.log(req.protocol)
+    console.log(req.method)
+    console.log(req.get("host"))
+    next()
+})
 connectDB();
 app.use("/api/products",productsRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).json({ massage: "Path Not Found"});
+    next()
+})
+
+app.use((err,req,res,next) => {
+    res.status(err.status || 500).json({massage: err.massage ||  "Internal Server Error"  });
+})
 // PORT localhost
 const PORT=8000;
 app.listen(PORT,() => console.log(`test ${PORT}`)
